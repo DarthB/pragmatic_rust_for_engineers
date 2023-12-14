3d6b60a62b2dee9da1b757af35f5e67cdea2d650
Tim Janus <tim@janus.rs>
2023-12-14 20:16:27 +0100

    wip: Save a png and handle result, concludes task P.2 and P.3

--- haber_bosch/src/v2_main_plot.rs
+++ haber_bosch/src/v2_main_plot.rs
@@ -1,9 +1,15 @@
 use std::error::Error;
-
+use plotters::prelude::*;
 
 fn main() -> Result<(), Box<dyn Error>>
 {
     println!("Hello Plot!");
+    let da = BitMapBackend::new("my.png", (800,600)).into_drawing_area();
+    da.fill(&RED)?;
+    match da.present() {
+        Ok(_) => {},
+        Err(_) => println!("Could not save file"),
+    }
 
     Ok(())
 }
