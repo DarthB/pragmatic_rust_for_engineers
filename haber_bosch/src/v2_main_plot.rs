93833fd6bc2c378a16a84353fa0a881e27537fbd
Tim Janus <tim@janus.rs>
2023-12-14 20:51:41 +0100

    wip: Plotting the cubic function concludes P.4 and P.5

--- haber_bosch/src/v2_main_plot.rs
+++ haber_bosch/src/v2_main_plot.rs
@@ -5,7 +5,22 @@ fn main() -> Result<(), Box<dyn Error>>
 {
     println!("Hello Plot!");
     let da = BitMapBackend::new("my.png", (800,600)).into_drawing_area();
-    da.fill(&RED)?;
+    da.fill(&WHITE)?;
+
+    let mut chart = ChartBuilder::on(&da)
+        .margin(12)
+        .set_left_and_bottom_label_area_size(40)
+        .build_cartesian_2d(-3f32..3f32, -30f32..30f32)?;
+
+    chart.configure_mesh().x_desc("outside data").y_desc("magic numbers").draw()?;
+
+    let coords = (-300..300).into_iter()
+        .map(|x| x as f32 / 100.)
+        .map(|x| (x, x*x*x));
+
+    let coords: Vec<(f32, f32)> = coords.collect();
+    chart.draw_series(LineSeries::new(coords, &BLACK))?;
+
     match da.present() {
         Ok(_) => {},
         Err(_) => println!("Could not save file"),
