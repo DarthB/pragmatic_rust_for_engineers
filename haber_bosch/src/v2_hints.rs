62c479bbff03cf9d757c4a551efd63ad932acd53
Tim Janus <tim@janus.rs>
2023-12-14 16:12:03 +0100

    hint: Simulate and visualize a multi-bed Haber-Bosch Reactor

--- haber_bosch/src/v2_hints.rs
+++ haber_bosch/src/v2_hints.rs
@@ -38,3 +38,75 @@ pub const KMIR_B2_TS: f64 = 400. + 273.; // [K°] Starting temperature
 pub const KMIR_B2_TR: f64 = 7.5; // [K°] Raise of temperature
 pub const KMIR_B2_TM: f64 = 490. + 237.; // [K°] Max temperature (constants invalid out of this are)
 pub const KMIR_B2_B: f64 = -2.708; // beta
+
+// The function that calculates our more sophistcated model
+// Feel free to copy this block instead of implementing it on your own.
+// Check for the variables that need to be provided by your model, therefore see self.*, e.g. self.beta
+/*
+fn system(&self, _x: f64, y: &State, dy: &mut State) {
+    // use named variables
+    let (n2, h2, nh3) = (y[0], y[1], y[2]);
+    // y[3] and y[4] are inerts they don't change.
+    let temp = y[5];
+
+    // intermediate calculations for reaction rates
+    let k = self.big_a * (-self.ea / (R * temp)).exp();
+    let log10_ka = self.beta * temp.log10() - 5.519265e-5 * temp
+        + 1.848863e-7 * temp.powi(2)
+        + (2001.6 / temp)
+        + 2.6899;
+    let ka = 10f64.powf(log10_ka);
+
+    let t1 = ka.powi(2) * n2 * (h2.powi(3) / nh3.powi(2)).powf(self.alpha);
+    let t2 = (nh3.powi(2) / h2.powi(3)).powf(1. - self.alpha);
+
+    // reaction rates based on provided model
+    let rnh3 = k * (t1 - t2);
+    let rn2 = rnh3 / 2.;
+    let rh2 = rnh3 / 2. * 3.;
+
+    // derivatives for usage in the ODE solver
+    // components:
+    dy[0] = -rn2;
+    dy[1] = -rh2;
+    dy[2] = rnh3;
+    dy[3] = 0.;
+    dy[4] = 0.;
+
+    // temperature
+    dy[5] = self.t_slope;
+    dy[5] = if y[5] < self.t_max {
+        dy[5]
+    } else {
+        self.t_max - y[5]
+    };
+
+    // calculate fugacity components
+    let mut fug = State::default();
+    fug = fug.add_scalar(1.); // vector containing ones
+
+    // helper variables:
+    let (t, p) = (temp, self.pressure);
+    let t_squared = t.powi(2);
+    let p_squared = p.powi(2);
+
+    // fugacity calculations as described in model:
+    fug[0] = 0.93431737 + 0.3101804 * 10f64.powi(-3) * t + 0.295896 * 10f64.powi(-3) * p
+        - 0.2707279 * 10f64.powi(-6) * t_squared
+        + 0.4775207 * 10f64.powi(-6) * p_squared;
+
+    fug[1] = E.powf(
+        E.powf(-3.84027 * t.powf(1.25) + 0.541) * p
+            - E.powf(-0.012637 * t.powf(0.5) - 15.980) * p.powf(2.0)
+            + 300.0 * E.powf(-0.0119017 * t - 5.941) * (E.powf(-p / 300.0) - 1.0),
+    );
+
+    fug[2] = 0.1438996 + 0.2028538 * 10f64.powi(-2) * t
+        - 0.448762 * 10f64.powi(-3) * p
+        - 0.1142945 * 10f64.powi(-5) * t_squared
+        + 0.2761216 * 10f64.powi(-6) * p_squared;
+
+    // component-wise multiplication
+    *dy = dy.component_mul(&fug);
+}
+*/
