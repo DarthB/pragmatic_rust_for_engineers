020b0b53f3ba1d68aadea1ab35eba6dffba3c5f3
Tim Janus <tim@janus.rs>
2024-02-13 14:55:29 +0100

    wip add first interactivity with event handling and updatePlot

--- haber_bosch/www/bootstrap.js
+++ haber_bosch/www/bootstrap.js
@@ -1,13 +1,14 @@
 init();
 
 async function init() {
-    const [{hook_panic_handler, test_method, default: init}, {main, setup}] = await Promise.all([
+    const [{WebChart, hook_panic_handler, test_method, default: init}, {main, setup}] = await Promise.all([
         import("../pkg/haber_bosch.js"),
         import("./index.js"),
     ]);
     await init();
     
     hook_panic_handler();
+    setup(WebChart);
     test_method(); // check bindings of JS outputs into Rust
     main();
 }
